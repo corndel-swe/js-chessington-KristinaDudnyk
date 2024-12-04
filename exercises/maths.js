35411926985c3249667c6709c717d2d165ff9342
KristinaDudnyk <kristina.dudnyk20@gmail.com>
2024-12-04 14:02:13 +0000

    exercise

--- exercises/maths.js
+++ exercises/maths.js
@@ -1,1 +1,10 @@
-export class Maths {}
+export class Maths {
+  static PI = 3.14;
+  static max(a, b) {
+    if (a > b) {
+      return a;
+    } else {
+      return b;
+    }
+  }
+}
--- exercises/shapes.js
+++ exercises/shapes.js
@@ -1,72 +1,89 @@
 export class Shape {
   area() {
-    throw new Error('This method should be overwritten')
+    throw new Error("This method should be overwritten");
   }
 
   perimeter() {
-    throw new Error('This method should be overwritten')
+    throw new Error("This method should be overwritten");
   }
 }
 
 // Exercise 3: Make Rectangle, Triangle, and Circle child classes of Shape
 // And override the getArea and getPerimeter methods
 
-export class Rectangle {
-  #width
-  #height
+export class Rectangle extends Shape {
+  #width;
+  #height;
 
   constructor(width, height) {
-    this.#width = width
-    this.#height = height
+    super();
+    this.#width = width;
+    this.#height = height;
   }
 
   area() {
-    return this.#width * this.#height
+    return this.#width * this.#height;
   }
 
   perimeter() {
-    return 2 * (this.#width + this.#height)
+    return 2 * (this.#width + this.#height);
   }
 
   width() {
-    return this.#width
+    return this.#width;
   }
 
   height() {
-    return this.#height
+    return this.#height;
   }
 }
 
-export class RightTriangle {
-  #base
-  #height
+export class RightTriangle extends Shape {
+  #base;
+  #height;
 
   constructor(base, height) {
-    this.#base = base
-    this.#height = height
+    super();
+    this.#base = base;
+    this.#height = height;
+  }
+
+  area() {
+    return (this.#base * this.#height) / 2;
+  }
+  perimeter() {
+    return this.#base + this.#height + this.hypotenuse();
   }
 
   base() {
-    return this.#base
+    return this.#base;
   }
 
   height() {
-    return this.#height
+    return this.#height;
   }
 
   hypotenuse() {
-    return Math.sqrt(this.#base ** 2 + this.#height ** 2)
+    return Math.sqrt(this.#base ** 2 + this.#height ** 2);
   }
 }
 
-export class Circle {
-  #radius
+export class Circle extends Shape {
+  #radius;
 
   constructor(radius) {
-    this.#radius = radius
+    super();
+    this.#radius = radius;
   }
 
   radius() {
-    return this.#radius
+    return this.#radius;
+  }
+
+  area() {
+    return Math.PI * this.#radius * this.#radius;
+  }
+  perimeter() {
+    return 2 * Math.PI * this.#radius;
   }
 }
